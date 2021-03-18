e3711fbe0b9c18b37dc561cf2a4597e16b2042b5
Ryan <71246429+ryan-bitmax@users.noreply.github.com>
2021-03-18 14:50:52 +0800

    update slate

--- lib/monokai_sublime_slate.rb
+++ lib/monokai_sublime_slate.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 # this is based on https://github.com/rouge-ruby/rouge/blob/master/lib/rouge/themes/monokai_sublime.rb
-# but without the added background, and changed styling for JSON keys to be soft_green instead of white
+# but without the added background, and changed styling for JSON keys to be soft_yellow instead of white
 
 module Rouge
     module Themes
@@ -11,18 +11,18 @@ module Rouge
 
         palette :black          => '#000000'
         palette :bright_green   => '#a6e22e'
-        palette :bright_yellow  => '#f8c455'
+        palette :bright_pink    => '#f92672'
         palette :carmine        => '#960050'
         palette :dark           => '#49483e'
         palette :dark_grey      => '#888888'
         palette :dark_red       => '#aa0000'
-        palette :dimgrey        => '#999999'
+        palette :dimgrey        => '#75715e'
         palette :emperor        => '#555555'
         palette :grey           => '#999999'
         palette :light_grey     => '#aaaaaa'
-        palette :light_orange   => '#f08c49'
+        palette :light_violet   => '#ae81ff'
         palette :soft_cyan      => '#66d9ef'
-        palette :soft_green     => '#7ec699'
+        palette :soft_yellow    => '#e6db74'
         palette :very_dark      => '#1e0010'
         palette :whitish        => '#f8f8f2'
         palette :orange         => '#f6aa11'
@@ -42,7 +42,7 @@ module Rouge
               Comment,                          :fg => :dimgrey
         style Error,
               Generic::Error,
-              Generic::Traceback,               :fg => :dimgrey
+              Generic::Traceback,               :fg => :carmine
         style Generic::Deleted,
               Generic::Inserted,
               Generic::Emph,                    :fg => :dark
@@ -59,7 +59,7 @@ module Rouge
               Literal::Number,
               Literal::String::Char,
               Literal::String::Escape,
-              Literal::String::Symbol,          :fg => :light_orange
+              Literal::String::Symbol,          :fg => :light_violet
         style Literal::String::Doc,
               Literal::String::Double,
               Literal::String::Backtick,
@@ -67,7 +67,7 @@ module Rouge
               Literal::String::Interpol,
               Literal::String::Other,
               Literal::String::Single,
-              Literal::String,                  :fg => :soft_green
+              Literal::String,                  :fg => :soft_yellow
         style Name::Attribute,
               Name::Class,
               Name::Decorator,
@@ -83,13 +83,13 @@ module Rouge
               Text::Whitespace,
               Text,
               Name,                             :fg => :white
-        style Name::Label,                      :fg => :bright_yellow
+        style Name::Label,                      :fg => :bright_pink
         style Operator::Word,
               Name::Tag,
               Keyword,
               Keyword::Namespace,
               Keyword::Pseudo,
-              Operator,                         :fg => :bright_yellow
+              Operator,                         :fg => :bright_pink
       end
     end
   end
